69447ccc62bd4cbfbf88efdae8ea09aa2efbebc0
Nicko Satria Utama <nicko.su@hotmail.com>
2023-12-20 16:50:15 +0800

    Starting commit

---
+++ components/Navigation2.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="site-main-menu">
+        <ul>
+            <li class="has-children">
+                <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/about">About Us</NuxtLink>
+            </li>
+            <li class="has-children">
+                <NuxtLink to="/service">Service</NuxtLink>
+                <ul class="sub-menu">
+                    <li>
+                        <NuxtLink to="/services/travel">Travel</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/services/meeting">Meeting</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/services/development">Software Development</NuxtLink>
+                    </li>
+                </ul>
+            </li>
+            <!-- <li class="has-children">
+                <NuxtLink to="/project">Products</NuxtLink>
+                <ul class="sub-menu">
+                    <li>
+                        <NuxtLink to="/project">Project</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/project/project-details">Project Details</NuxtLink>
+                    </li>
+                </ul>
+            </li> -->
+            <!-- <li class="has-children">
+                <NuxtLink to="/blog">Blog</NuxtLink>
+                <ul class="sub-menu">
+                    <li>
+                        <NuxtLink to="/blog">Blog Grid</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/blog/blog-classic">Blog Classic</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/blog/blog-details">Blog Details</NuxtLink>
+                    </li>
+                </ul>
+            </li> -->
+            <li>
+                <NuxtLink to="/contact-us">Contact Us</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main-container">
+        <TheHeader2 />
+        <OffCanvasMobileMenu />
+        <SearchPopup />
+        <HeroSliderOne />
+        <AboutOne />
+        <FeatureOne :style="{backgroundColor: '#f8faff'}" />
+        <SuccessStory />
+        <PortfolioGallery />
+        <BlogSectionOne />
+        <Newsletter />
+        <CallToActionOne2 />
+        <TheFooter2 />
+    </div>
+</template>
+
+<script>
+    export default {
+        head() {
+            return {
+                title: 'Bokong Semok'
+            }
+        },
+    };
+</script>
+
+
